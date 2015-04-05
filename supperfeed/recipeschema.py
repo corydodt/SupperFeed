cfbd2a918f6349f8902497404dc5de76cc9205cb
Cory Dodt <corydodt@gmail.com>
2015-04-04 20:03:58 -0700

    even more robust splitting

--- supperfeed/recipeschema.py
+++ supperfeed/recipeschema.py
@@ -20,24 +20,37 @@ from supperfeed.build import ITEM_SEPARATOR
 MICROFORMAT_RECIPE = 'http://schema.org/Recipe'
 
 
-def preCleanIngredients(soup, separator=ITEM_SEPARATOR):
+# list tags that cannot contain text content, and put separators after instead
+# of inside
+POSTFIX_TAGS = 'br img script hr'.split()
+
+def insertSeparator(soup, el, separator):
     """
-    Break up ingredients formatted by the method of using an ingredient class
+    Find the last text eleemnt of el and add the separator there
+    """
+    if el.name in POSTFIX_TAGS:
+        el.insert_after(soup.new_string(separator))
+    else:
+        lastText = el.find_all(text=True)
+        if lastText:
+            repl = soup.new_string(lastText[-1] + separator)
+            lastText[-1].replace_with(repl)
+
+def separateByClass(soup, parent, classes, separator=ITEM_SEPARATOR):
+    """
+    Break up ingredients or instructions formatted by the method of using a class,
     by appending the separator character
     """
-    for ing in soup.find_all(None, "ingredient"):
-        ing.insert_after(soup.new_string(separator))
+    for thing in parent.find_all(None, class_=classes):
+        thing.insert_after(soup.new_string(separator))
 
-def preCleanInstructions(soup, splitter='br', separator=ITEM_SEPARATOR):
+def separateByTag(soup, parent, splitters, separator=ITEM_SEPARATOR):
     """
-    Break up instructions formatted by the method of inserting a br
+    Break up instructions formatted by the method of using a particular tag
     between steps, by appending the separator character
     """
-    instructionses = soup.find_all(None, itemprop="recipeInstructions")
-    for ins in instructionses:
-        for sub in ins.contents:
-            if sub.name == splitter:
-                sub.insert_after(soup.new_string(separator))
+    for splittee in parent.find_all(name=splitters):
+        insertSeparator(soup, splittee, separator)
 
 class Options(usage.Options):
     synopsis = "recipeschema 'http://....'"
@@ -49,18 +62,32 @@ class Options(usage.Options):
         self['urls'] = urls
 
     def showData(self, recipes):
+        """
+        Convert data to json and write to stdout
+        """
         for recipe in recipes:
             json.dump(recipe, self.stdout, indent=2)
             print
             print '/'  + '*' * 50 + '/'
 
     def cleanData(self, recipes, url):
+        """
+        Remove junk spacing from structured data
+        """
         ret = []
         for recipe in recipes:
             props = recipe['properties']
             for k, vals in props.items():
                 new = []
                 for v in vals:
+                    if type(v) is dict:
+                        if v.has_key('properties'):
+                            vv = ''
+                            for prop in v['properties'].values():
+                                vv += prop[0]
+                            v = vv
+                        else:
+                            continue
                     lines = v.splitlines()
                     vv = ' '.join([line.strip() for line in lines]).strip()
                     new.append(vv)
@@ -70,11 +97,20 @@ class Options(usage.Options):
         return ret
 
     def consumeData(self, data):
+        """
+        Parse the microdata into structured data
+        """
         ret = []
 
         soup = BeautifulSoup(StringIO(data))
-        preCleanIngredients(soup)
-        preCleanInstructions(soup)
+        ingredientses = soup.find_all(None, itemprop='ingredients')
+        for ing in ingredientses:
+            separateByClass(soup, ing, "ingredient")
+            separateByTag(soup, ing, ['br', 'tr', 'li'])
+        instructionses = soup.find_all(None, itemprop="recipeInstructions")
+        for ins in instructionses:
+            separateByClass(soup, ins, "instruction")
+            separateByTag(soup, ins, ['br', 'tr', 'li'])
         workingDocument = StringIO(soup.encode('utf-8'))
 
         items = microdata.get_items(workingDocument)
